1a8b0b0b54092c9bf73198faac79ada4f8ca0934
Mac Shu <mac.shu@outlook.com>
2023-04-03 11:30:02 +0800

    fix: remove page index.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  hooks: {
-    'pages:extend'(pages) {
-      // Add routes
-      if (process.env.npm_package_name !== 'nuxt-3-main-app') { 
-        pages.push({
-          name: 'Homepage',
-          path: '/',
-          file: '~/pages/index.vue'
-        })
+  // hooks: {
+  //   'pages:extend'(pages) {
+  //     // Add routes
+  //     if (process.env.npm_package_name !== 'nuxt-3-main-app') { 
+  //       pages.push({
+  //         name: 'Homepage',
+  //         path: '/',
+  //         file: '~/pages/index.vue'
+  //       })
       
-        // remove routes
-        function removePagesMatching(pattern: RegExp, pages: NuxtPage[] = []) {
-          const pagesToRemove = []
-          for (const page of pages) {
-            if (pattern.test(page.file)) {
-              pagesToRemove.push(page)
-            } else {
-              removePagesMatching(pattern, page.children)
-            }
-          }
-          for (const page of pagesToRemove) {
-            pages.splice(pages.indexOf(page), 1)
-          }
-        }
-        removePagesMatching(/\.ts$/, pages)
-      }
-    }
-  },
+  //       // remove routes
+  //       function removePagesMatching(pattern: RegExp, pages: NuxtPage[] = []) {
+  //         const pagesToRemove = []
+  //         for (const page of pages) {
+  //           if (pattern.test(page.file)) {
+  //             pagesToRemove.push(page)
+  //           } else {
+  //             removePagesMatching(pattern, page.children)
+  //           }
+  //         }
+  //         for (const page of pagesToRemove) {
+  //           pages.splice(pages.indexOf(page), 1)
+  //         }
+  //       }
+  //       removePagesMatching(/\.ts$/, pages)
+  //     }
+  //   }
+  // },
   runtimeConfig: {
     public: {
       theme: {
